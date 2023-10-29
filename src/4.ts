f1e80c3aad62e0c0a9eeddcf9aa6afa0959e6ce2
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-10-29 13:56:34 +0200

    start4

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,54 @@
-const key = new Key();
 
+class Key {
+    private signature: number;
+    constructor () {
+        this.signature = Math.random();
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
+  
+class Person {
+     constructor (private key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+    abstract openDoor(key: Key): void;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log('Wolcome home');
+        } else {
+            console.log('The doors are closed');
+        }
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature()===this.key.getSignature()) {
+            this.door = true;
+            console.log('The doors are open');
+        } else {
+            console.log('The doors are closen. Try again');
+        }
+    }
+}
+
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
